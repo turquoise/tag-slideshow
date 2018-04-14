b1438ada4715018826c66e45ebc7b3b761670c06
M Gosschalk <monica.gosschalk@btinternet.com>
2018-04-14 22:07:03 +0100

    second commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ViewChild, AfterViewInit, ElementRef } from '@angular/core';
+
+import {trigger, transition, style, animate, query, stagger} from '@angular/animations';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  animations: [
+
+  ]
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements OnInit {
+
+
+  slide1 = [
+    '../assets/Img/edward-cisneros-411006-unsplash.jpg',
+    '../assets/Img/nainoa-shizuru-80385-unsplash.jpg',
+    '../assets/Img/noiseporn-247586-unsplash.jpg'
+  ];
+  slide2 = [
+  ];
+  slide3 = [
+  ];
+
+  ngOnInit() {
+
+  }
+
+
+
+
+
 }
